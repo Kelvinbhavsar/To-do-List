8eb0749a52c4403cd59ba65cfeb338c01106130f
niyam <niyam4103@gmail.com>
2024-08-10 13:43:43 +0530

    added submitted stream in taskscreen

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
-import 'package:user_todo/screens/google_sign_in_screen.dart';
 import 'package:user_todo/screens/splash_screen.dart';
 
 void main() async {
--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -53,76 +53,160 @@ class _TaskScreenState extends State<TaskScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection('tasks')
-            .where('email', isEqualTo: kFirebase.currentUser!.email)
-            .where('submittime', isEqualTo: '-')
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            var data = snapshot.data!.docs;
-            return data.length == 0
-                ? Center(
-                    child: Text(
-                    "No Task Available for You",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-                  ))
-                : ListView.builder(
-                    itemCount: data.length,
-                    itemBuilder: (context, index) {
-                      Timestamp ts = data[index]['createtime'];
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection('tasks')
+                  .where('email', isEqualTo: kFirebase.currentUser!.email)
+                  .where('submittime', isEqualTo: '-')
+                  .orderBy('createtime', descending: true)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  var data = snapshot.data!.docs;
+                  return data.length == 0
+                      ? Center(
+                          child: Text(
+                          "No Task Available for You",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w600),
+                        ))
+                      : ListView.builder(
+                          itemCount: data.length,
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          itemBuilder: (context, index) {
+                            Timestamp ts = data[index]['createtime'];
 
-                      /* return data[index]['submittime'] != '-'
-                          ? SizedBox()
-                          : Card*/
-                      return Card(
-                        margin: EdgeInsets.all(8.0),
-                        child: ListTile(
-                          contentPadding: EdgeInsets.all(16.0),
-                          title: Text(data[index]['taskname']),
-                          subtitle: Text(
-                              'assigned on : ${ts.toDate().day} , ${months[ts.toDate().month - 1]}'),
-                          trailing: IconButton(
-                            icon: const Icon(Icons.radio_button_unchecked),
-                            onPressed: () {
-                              showDialog(
-                                context: context,
-                                builder: (context) => AlertDialog(
+                            /* return data[index]['submittime'] != '-'
+                                ? SizedBox()
+                                : Card*/
+                            return Card(
+                              margin: EdgeInsets.all(8.0),
+                              child: ListTile(
+                                contentPadding: EdgeInsets.all(16.0),
+                                title: Text(data[index]['taskname']),
+                                subtitle: Text(
+                                    'assigned on : ${ts.toDate().day} , ${months[ts.toDate().month - 1]}'),
+                                trailing: IconButton(
+                                  icon:
+                                      const Icon(Icons.radio_button_unchecked),
+                                  onPressed: () {
+                                    showDialog(
+                                      context: context,
+                                      builder: (context) => AlertDialog(
+                                        title: Text(
+                                            "Are you sure you want to submit it?"),
+                                        content: Text(
+                                            "Task : ${data[index]['taskname']}"),
+                                        actions: [
+                                          ElevatedButton(
+                                              onPressed: () {
+                                                Navigator.pop(context);
+                                              },
+                                              child: Text("Cancel")),
+                                          ElevatedButton(
+                                              onPressed: () async {
+                                                await taskCollection
+                                                    .doc(
+                                                        convertSpaceToUnderScore(
+                                                            data[index]
+                                                                ['taskname']))
+                                                    .update({
+                                                  'submittime': Timestamp.now()
+                                                }).then(
+                                                  (value) =>
+                                                      Navigator.pop(context),
+                                                );
+                                              },
+                                              child: Text("Yes")),
+                                        ],
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                }
+                return Container();
+              },
+            ),
+            Divider(),
+            StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection('tasks')
+                  .where('email', isEqualTo: kFirebase.currentUser!.email)
+                  .where('submittime', isNotEqualTo: '-')
+                  .orderBy('createtime', descending: true)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return CircularProgressIndicator();
+                }
+                var data2 = snapshot.data!.docs;
+                return data2.length == 0
+                    ? Text("No Task Submitted Yet")
+                    : Column(
+                        children: [
+                          Align(
+                            alignment: Alignment(-0.8, 0),
+                            child: Text(
+                              "Submitted Tasks",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                          ListView.builder(
+                            shrinkWrap:
+                                true, // Use this to wrap content inside Column
+                            physics:
+                                NeverScrollableScrollPhysics(), // Disable scrolling for the ListView
+                            itemCount: data2.length,
+                            itemBuilder: (context, index) {
+                              Timestamp ts = data2[index]['createtime'];
+                              return Card(
+                                margin: const EdgeInsets.symmetric(
+                                    vertical: 8.0, horizontal: 16.0),
+                                child: ListTile(
                                   title: Text(
-                                      "Are you sure you want to submit it?"),
-                                  content:
-                                      Text("Task : ${data[index]['taskname']}"),
-                                  actions: [
-                                    ElevatedButton(
-                                        onPressed: () {
-                                          Navigator.pop(context);
-                                        },
-                                        child: Text("Cancel")),
-                                    ElevatedButton(
-                                        onPressed: () async {
-                                          await taskCollection
-                                              .doc(convertSpaceToUnderScore(
-                                                  data[index]['taskname']))
-                                              .update({
-                                            'submittime': Timestamp.now()
-                                          }).then(
-                                            (value) => Navigator.pop(context),
-                                          );
-                                        },
-                                        child: Text("Yes")),
-                                  ],
+                                    data2[index]['taskname']!,
+                                    style: const TextStyle(
+                                      decoration: TextDecoration.lineThrough,
+                                    ),
+                                  ),
+                                  subtitle: Align(
+                                    alignment: Alignment.bottomRight,
+                                    child: Text(
+                                        'assigned on : ${ts.toDate().day} , ${months[ts.toDate().month - 1]}'),
+                                  ),
+
+                                  /// delete button for submitted tasks
+                                  /* trailing: IconButton(
+                                    icon: const Icon(Icons.delete),
+                                    onPressed: () {
+                                      setState(() async {
+                                        await FirebaseFirestore.instance
+                                            .collection('tasks')
+                                            .doc(data2[index]['taskname']
+                                                .toString()
+                                                .replaceAll(' ', '_'))
+                                            .delete();
+                                      });
+                                    },
+                                  ),*/
                                 ),
                               );
                             },
                           ),
-                        ),
+                        ],
                       );
-                    },
-                  );
-          }
-          return Container();
-        },
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
